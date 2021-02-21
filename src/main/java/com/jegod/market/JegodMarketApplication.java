9dfb2fd541fd1c4fcd4bf8a24f1b8cfaa55602f4
“SergioLeguizamon” <sergioleguizamon_b@hotmail.com>
2021-02-21 14:29:12 -0500

    project creation

---
+++ src/main/java/com/jegod/market/JegodMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jegod.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JegodMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JegodMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jegod/market/JegodMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jegod.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JegodMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
