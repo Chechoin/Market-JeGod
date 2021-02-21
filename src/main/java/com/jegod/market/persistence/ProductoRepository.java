22a47610e788557ce91ce1d676e6852a28f82b1a
“SergioLeguizamon” <sergioleguizamon_b@hotmail.com>
2021-02-21 17:54:27 -0500

    Clase 20 Implementar la anotación @Repository

--- src/main/java/com/jegod/market/persistence/ProductoRepository.java
+++ src/main/java/com/jegod/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.jegod.market.persistence;
 
 import com.jegod.market.persistence.crud.ProductoCrudRepository;
 import com.jegod.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+//Con @Repository se le dice a spring que esta clase interactua con la base de datos
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -17,6 +19,18 @@ public class ProductoRepository {
     }
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
-
     }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete (int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+
 }
